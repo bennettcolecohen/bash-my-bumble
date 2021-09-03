1031d3d5ab0a7f5582cf3114b3814ce33aac1fff
bennettccohen@gmail.com <bennettcohen@Bennetts-Air.hsd1.ca.comcast.net>
2021-09-02 21:54:34 -0700

    server views working

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 // Load Node modules
 var express = require('express');
+const ejs = require('ejs');
 // Initialise Express
 var app = express();
 // Render static files
 app.use(express.static('public'));
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
 // Port website will run on
 app.listen(8080);
 
+// *** GET Routes - display pages ***
+
+// HOME ROUTE
+app.get('/', function (req, res) {
+    res.render('pages/home');
+});
+
+// LOGIN PROMPT
+app.get('/', function (req, res) {
+    res.render('pages/login_prompt');
+});
